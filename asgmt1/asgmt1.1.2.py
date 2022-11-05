0cc36cccb7af470cd27651bd37c1bcdb8e03ffa8
Yuuki <yh.works01@gmail.com>
2022-11-06 00:14:11 +0900

    asgmt1.1.2.pyの修正

--- asgmt1/asgmt1.1.2.py
+++ asgmt1/asgmt1.1.2.py
@@ -1,6 +1,8 @@
 from asgmt1.function.calcMV4 import calcMV4
 from asgmt1.mytyping.typing import VectorS
+import numpy as np
 
+# (v0, v1, v2, v3)^Tを生成
 def vvecGenerate(n0: float, n1: float, n2: float) -> VectorS:
     v0 = -n0*n1*n2
     v1 = n0*n1+n1*n2+n2*n0
@@ -20,10 +22,25 @@ def main():
             [1, x2, x2**2, x2**3],
             [1, x3, x3**2, x3**3]]
 
-    print("f0:" + str(calcMV4(vmat, vvecGenerate(x1, x2, x3))))
-    print("f1:" + str(calcMV4(vmat, vvecGenerate(x0, x2, x3))))
-    print("f2:" + str(calcMV4(vmat, vvecGenerate(x1, x0, x3))))
-    print("f3:" + str(calcMV4(vmat, vvecGenerate(x1, x2, x0))))
+    # 計算結果を表示
+    print(f'coef_f0: {vvecGenerate(x1, x2, x3)}\n'
+          f'coef_f1: {vvecGenerate(x0, x2, x3)}\n'
+          f'coef_f2: {vvecGenerate(x1, x0, x3)}\n'
+          f'coef_f3: {vvecGenerate(x1, x2, x0)}\n\n'
+          f'A*f0: {calcMV4(vmat, vvecGenerate(x1, x2, x3))}\n'
+          f'A*f1: {calcMV4(vmat, vvecGenerate(x0, x2, x3))}\n'
+          f'A:f2: {calcMV4(vmat, vvecGenerate(x1, x0, x3))}\n'
+          f'A*f3: {calcMV4(vmat, vvecGenerate(x1, x2, x0))}\n')
+
+    # A*[f0, f1, f2, f3]が単位行列になるように対応する成分で除算
+    #nmdはnormalizedの略称
+    nmd_coef_f0 = np.array(vvecGenerate(x1, x2, x3)) / calcMV4(vmat, vvecGenerate(x1, x2, x3))[0]
+    nmd_coef_f1 = np.array(vvecGenerate(x0, x2, x3)) / calcMV4(vmat, vvecGenerate(x0, x2, x3))[1]
+    nmd_coef_f2 = np.array(vvecGenerate(x1, x0, x3)) / calcMV4(vmat, vvecGenerate(x1, x0, x3))[2]
+    nmd_coef_f3 = np.array(vvecGenerate(x1, x2, x0)) / calcMV4(vmat, vvecGenerate(x1, x2, x0))[3]
+
+    vmat_inv = np.array([nmd_coef_f0, nmd_coef_f1, nmd_coef_f2, nmd_coef_f3]).T
+    print(vmat_inv)
 
 
 if __name__ == '__main__':
