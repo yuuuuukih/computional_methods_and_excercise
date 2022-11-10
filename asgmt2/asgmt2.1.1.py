6e202a9a6899ea4c84d9ce883d397fd7083c689b
Yuuki <yh.works01@gmail.com>
2022-11-10 16:30:39 +0900

    asgmt2.1.1.py completed

--- asgmt2/asgmt2.1.1.py
+++ asgmt2/asgmt2.1.1.py
@@ -1,10 +1,14 @@
-from const_value import A
+import numpy as np
+from const_value import A, b1, alpha1
 from function.LU_decomposition import get_LU
+from function.substitution_method import calc_forward, calc_backward
 
 def main():
     L, U = get_LU(A)
-    print(L)
-    print(U)
+    y = calc_forward(L, b1)
+    x = calc_backward(U, y)
+    print(f'x: {x}')
+    print(f'(x-alpha1)^2: {np.linalg.norm(x-alpha1, ord=2)**2}')
 
 if __name__ == '__main__':
     main()
---
+++ asgmt2/function/substitution_method.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def basic_error_check(A, b):
+    n = len(A)
+    if len(b) != n:
+        print('Dimention Error!')
+        return
+
+    if len(A[0]) != n:
+        print('Matrix is not square!')
+
+# Aが上三角行列のときはlower=0
+def check_for_tringular_matrix(A, lower=1):
+    n = len(A)
+    name = 'lower' if lower == 1 else 'upper'
+    if lower != 1:
+        A = A.T
+
+    # Aが下三角行列か否かのチェック
+    for i in range(n):
+        for j in range(i+1, n):
+            if A[i][j] != 0:
+                print(f'Matrix is not {name} triangular matrix!')
+                return
+            j += 1
+        i += 1
+
+
+# 下三角行列Lをとって前進代入法で解く
+def calc_forward(L, b):
+    n = len(L)
+    basic_error_check(L, b)
+
+    # Aが下三角行列か否かのチェック
+    check_for_tringular_matrix(L)
+
+    x = np.zeros(n)
+
+    for i in range(n):
+        x[i] = b[i]
+        for j in range(i):
+            x[i] -= L[i][j] * x[j]
+            j += 1
+        x[i] /= L[i][i]
+        i += 1
+
+    return x
+
+# 上三角行列Uをとって後退代入法で解く
+def calc_backward(U, b):
+    n = len(U)
+    basic_error_check(U, b)
+
+    # 上三角行列のチェック
+    check_for_tringular_matrix(U, 0)
+
+    x = np.zeros(n)
+    for k in range(n):
+        i = n-k-1
+        x[i] = b[i]
+        for j in range(k):
+            x[i] -= U[i][n-j-1] * x[n-j-1]
+            j += 1
+        x[i] /= U[i][i]
+        k += 1
+
+    return x
