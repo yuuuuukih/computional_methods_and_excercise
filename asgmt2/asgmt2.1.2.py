9dc8c660e6155815b4c85dd2cdea7ef050a0baf0
Yuuki <yh.works01@gmail.com>
2022-11-10 23:46:10 +0900

    asgmt2.1.2.py almost completed

--- asgmt2/asgmt2.1.2.py
+++ asgmt2/asgmt2.1.2.py
@@ -1,16 +1,28 @@
 import numpy as np
-from function.DLU_decomposition import get_DLU
-from const_value import A
-from function.calc_mat import calc_mat
-from function.inverse_mat import get_inverse_matrix
-from function.LU_decomposition import get_LU
+from const_value import A, b1
+from function.iterative_method import calc_iterative_method
+import matplotlib.pyplot as plt
 
 def main():
-    D, L, U = get_DLU(A)
+    output_jac = calc_iterative_method(A, b1)
+    output_gau = calc_iterative_method(A, b1, method='gauss_seidel')
+    output_sor = calc_iterative_method(A, b1, method='sor', omega=1.9)
 
-    inv_arr1 = get_inverse_matrix(A)
 
-    print(calc_mat(inv_arr1, A))
+    # 収束先を描画
+    plt.rcParams['font.family'] = "Meiryo"
+    fig = plt.figure(figsize=(4, 4), dpi=100)
+    fig.subplots_adjust(left=0.2, bottom=0.2)
+    ax = fig.add_subplot(1, 1, 1, xlabel='count', ylabel='r')
+    ax.grid(color="#eeeeee", which="both")
+    ax.set_axisbelow(True)
+    ax.set_yscale('log')
+
+    ax.scatter(np.arange(len(output_jac['err'])), output_jac['err'], label='jacobi', s=2)
+    ax.scatter(np.arange(len(output_gau['err'])), output_gau['err'], label='gauss_seidel', s=2)
+    ax.scatter(np.arange(len(output_sor['err'])), output_sor['err'], label='sor', s=2)
+    ax.legend(title='algorithm')
+    plt.show()
 
 if __name__ == '__main__':
     main()
--- asgmt2/function/iterative_method.py
+++ asgmt2/function/iterative_method.py
@@ -1,9 +1,15 @@
 import numpy as np
-from DLU_decomposition import get_DLU
+from .DLU_decomposition import get_DLU
+from .calc_mat import calc_mat
+from .calc_mat_vec import calc_mat_vec
+from .inverse_mat import get_inverse_matrix
+from tqdm import tqdm
 
-def calc_iterative_method(A, b, method='jacobi', omega=1.9):
+
+METHODS = ['jacobi', 'gauss_seidel', 'sor']
+def calc_iterative_method(A, b, method=METHODS[0], omega=1.9, EPS=1e-10, k_max=10000):
     # methodチェック
-    if method != 'jacobi' or 'gauss_seidel' or 'sor':
+    if not(method in METHODS):
         print('Method Error!')
         return
     if not(0 < omega < 2):
@@ -13,8 +19,29 @@ def calc_iterative_method(A, b, method='jacobi', omega=1.9):
     # M,N生成
     D, L, U = get_DLU(A)
     M = D if method == 'jacobi' else D + L if method == 'gauss_seidel' else (D + omega * L) / omega if method == 'sor' else np.array([])
-    N = A - M
+    N = M - A
 
     n = len(A)
-    x = np.full(n, np.random.randn())
+    B = calc_mat(get_inverse_matrix(M), N)
+    c = calc_mat_vec(get_inverse_matrix(M), b)
+
+    x = np.zeros(n).reshape((1, n))
+    r = np.array([np.linalg.norm(b - calc_mat_vec(A, x[0]), ord=2)])
+    for k in tqdm(range(k_max)):
+        x_next = calc_mat_vec(B, x[k]) + c
+        x = np.append(x, x_next).reshape((k+2, n))
+
+        r_next = np.linalg.norm(b - calc_mat_vec(A, x[-1]), ord=2)
+        r = np.append(r, r_next)
+
+        if r[-1] < EPS:
+            break
+        k += 1
+
+    output = {
+        'sol': x,
+        'err': r
+    }
+
+    return output
 
