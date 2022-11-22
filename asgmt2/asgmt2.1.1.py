1138b82fa7c55cdac220480a5a89dc9c94f93f66
Yuuki <yh.works01@gmail.com>
2022-11-22 23:39:38 +0900

    machine epsilon　追加

--- asgmt2/asgmt2.1.1.py
+++ asgmt2/asgmt2.1.1.py
@@ -2,12 +2,15 @@ import numpy as np
 from const_value import A, b1, alpha1
 from function.LU_decomposition import get_LU
 from function.substitution_method import calc_forward_backward_by_LU
+import sys
 
 def main():
     L, U = get_LU(A)
     x = calc_forward_backward_by_LU(L, U, b1)
     print(f'x: {x}')
     print(f'(x-alpha1)^2: {np.linalg.norm(x-alpha1, ord=2)**2}')
+
+    print(f'machine epsilon: {sys.float_info.epsilon}')
 
 if __name__ == '__main__':
     main()
